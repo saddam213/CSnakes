2fb366539546c0d37681511c4ce2bf01e697db16
sa_ddam213 <sa_ddam213@live.com>
2025-05-24 11:20:16 +1200

    Create runtime environments

---
+++ src/CSnakes.Runtime/PythonEnvironmentHelper.cs
@@ -0,0 +1,19 @@
+using CSnakes.Runtime.EnvironmentManagement;
+using CSnakes.Runtime.Locators;
+using CSnakes.Runtime.PackageManagement;
+using Microsoft.Extensions.Logging;
+
+namespace CSnakes.Runtime;
+public class PythonEnvironmentHelper
+{
+    public static IPythonEnvironment CreateEnvironment(string environmentName, string pythonPath = null, string modulePath = null, string requirements = "requirements.txt", string version = null, ILogger logger = default)
+    {
+        var pythonVersion = ServiceCollectionExtensions.ParsePythonVersion(version);
+        var environmentPath = Path.Join(modulePath, $".{environmentName}");
+        var pythonLocator = new FolderLocator(pythonPath, pythonVersion);
+        var packageInstaller = new PipInstaller(logger, requirements);
+        var environmentOptions = new PythonEnvironmentOptions(modulePath, []);
+        var environmentManager = new VenvEnvironmentManagement(logger, environmentPath, true);
+        return PythonEnvironment.GetPythonEnvironment([pythonLocator], [packageInstaller], environmentOptions, logger, environmentManager);
+    }
+}
